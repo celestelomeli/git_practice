9b935c008a1c9be77eabf320e0b581f95d1b9fb4
Celeste Lomeli <celestelomeli@Celestes-Air.attlocal.net>
2022-04-05 15:39:45 -0700

    add python greet program

---
+++ practice.py
@@ -0,0 +1,11 @@
+#This function receives user input for "name" and prints out a greeting plus user
+input
+
+def main():
+	name = input("Enter your name : ")
+	print(name)
+
+	print("How are you", name)
+
+main()
+
